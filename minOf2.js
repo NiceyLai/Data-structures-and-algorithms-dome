ac3a14a251acdc4186deef8097c52c72e15a5aa9
nicey <1919325005@qq.com>
2022-08-18 16:05:36 +0800

    1

--- minOf2.js
+++ minOf2.js
@@ -7,6 +7,16 @@
 // };
 
 // 第一次优化
-let minOf2 = (numbers) => (number[0] > number[1] ? number[1] : number[0]);
+// let minOf2 = (numbers) => (number[0] > number[1] ? number[1] : number[0]);
 
-// 第二次优化
+// 第二次优化：析构赋值
+let minOf2 = ([a, b]) => (a < b ? a : b)
+
+// 调用方法
+// 小白调用
+minOf2([2, 6]);
+console.log(minOf2([2, 6]));
+
+// 高手调用
+minOf2.call(null, [355, 190])
+console.log(minOf2.call(null, [355, 190]));
