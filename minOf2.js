181794ad1b8ee0e42217939bc4512b4ba5dd19d8
nicey <1919325005@qq.com>
2022-08-18 15:56:32 +0800

    优化

---
+++ minOf2.js
@@ -0,0 +1,12 @@
+// let minOf2 = function () {
+//   if (number[0] > number[1]) {
+//     return number[1];
+//   } else {
+//     return number[0];
+//   }
+// };
+
+// 第一次优化
+let minOf2 = (numbers) => (number[0] > number[1] ? number[1] : number[0]);
+
+// 第二次优化
