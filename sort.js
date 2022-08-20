5655d1156dee37795a45ed53e9fff9e6b4f285f6
nicey <1919325005@qq.com>
2022-08-20 13:39:47 +0800

    fix bug

--- sort.js
+++ sort.js
@@ -1,32 +1,24 @@
-let numbers = [12, 14, 6, 1, 5, 8, 7, 9];
+let numbers = [12, 9, 10, 5, 1];
 
-let min = (arr1) => {
-  if (arr1.length > 2) {
-    return min([arr1[0], min(arr1.slice(1))]);
+let min = (numbers) => {
+  if (numbers.length > 2) {
+    return min([numbers[0], min(numbers.slice(1))]);
   } else {
-    return Math.min.apply(null, arr1);
+    return Math.min.apply(null, numbers);
   }
 };
-min(numbers);
-let minIndex = (numbers1) => {
-  numbers1.indexOf(min(numbers));
-};
-minIndex(numbers);
-console.log(minIndex(numbers));
+
+let minIndex = (numbers) => numbers.indexOf(min(numbers));
 
 let sort = (numbers) => {
   if (numbers.length > 2) {
-    let index = minIndex(numbers);
-    let min = numbers[index];
-    console.log("0");
-    console.log(min);
-
-    // 从numbers删掉min
-    numbers.splice(index, 1);
+    let index = minIndex(numbers); // 拿到numbers最小的元素的下标
+    let min = numbers[index]; // 最小的元素
+    numbers.splice(index, 1); // 从numbers删掉最小的元素min
     return [min].concat(sort(numbers));
   } else {
     return numbers[0] < numbers[1] ? numbers : numbers.reverse();
   }
 };
-sort(numbers);
-// console.log(sort(numbers));
+let s = sort(numbers);
+console.log(s);
